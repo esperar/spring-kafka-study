d70252516dbf60f15b56f3759a44f7e7637d1503
aesperer <piyrw9754@gmail.com>
2023-06-28 14:50:27 +0900

    create :: kafka producer

---
+++ src/main/kotlin/esperer/kafka/service/KafkaProducer.kt
@@ -0,0 +1,22 @@
+package esperer.kafka.service
+
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class KafkaProducer(
+    private val kafkaTemplate: KafkaTemplate<String, String>
+) {
+
+    private val log by lazy { LoggerFactory.getLogger(this.javaClass.simpleName) }
+
+    companion object {
+        const val TOPIC = "esperer"
+    }
+
+    fun sendMessage(message: String){
+        log.info("Produce message: $message")
+        this.kafkaTemplate.send(TOPIC, message)
+    }
+}
